4a6f87b9f857daebb30251a2c037ecda7881ea92
Christian <christianperez.rbb@gmail.com>
2023-09-24 23:12:00 +0200

    Solucionado problema con Javascript, ahora ya funcionan los botones

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 let pantalla = document.getElementById("screen");
-let botonTriangulo = document.getElementById("triangulo");
-let botonIdPower = document.getElementById("idpower");
 
-botonTriangulo.addEventListener("click", (e) => {
-    if (e.target.id === "triangulo") {
-        pantalla.style.backgroundColor = "red";
-    } else if (e.target.id === 'idpower') {
-        pantalla.style.backgroundColor = "grey";
-    }
+document.addEventListener("click", (e) => {
+  if (e.target.id === "triangulo") {
+    pantalla.style.backgroundColor = "red";
+  } else if (e.target.id === "idpower") {
+    pantalla.style.backgroundColor = "pink";
+  }
 });
-
-
-
-
