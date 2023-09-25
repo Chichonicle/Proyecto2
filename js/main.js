5340fe5d8901b408bce637c7bbd79998386160da
Christian <christianperez.rbb@gmail.com>
2023-09-25 03:08:32 +0200

    Hago otra prueba para ver si ahora funcionan las imagenes en 'pages'

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ let pantallaEncendida = false;
 botonPower.addEventListener("click", () => {
   if (!pantallaEncendida) {
 
-    pantalla.style.backgroundImage = "url('../img/on.jpg')";
+    pantalla.style.backgroundImage = "url('./img/on.jpg')";
     luz.style.backgroundColor = "yellow";
     pantallaEncendida = true;
     pantalla.addEventListener("animationiteration", () => {
       pantalla.style.animationPlayState = "paused";
     });
   } else {
-    
+
     pantalla.style.backgroundImage = "none";
     luz.style.backgroundColor = "";
     pantallaEncendida = false;
@@ -24,13 +24,13 @@ document.addEventListener("click", (e) => {
 
   if (pantallaEncendida) {
     if (e.target.id === "triangulo") {
-      pantalla.style.backgroundImage = "url('../img/oliver.gif')";
+      pantalla.style.backgroundImage = "url('./img/oliver.gif')";
     } else if (e.target.id === "cuadrado") {
-      pantalla.style.backgroundImage = "url('../img/mk.gif')";
+      pantalla.style.backgroundImage = "url('./img/mk.gif')";
     } else if (e.target.id === "circulo") {
-      pantalla.style.backgroundImage = "url('../img/sonic.gif')";
+      pantalla.style.backgroundImage = "url('./img/sonic.gif')";
     } else if (e.target.id === "x") {
-      pantalla.style.backgroundImage = "url('../img/car.gif')";
+      pantalla.style.backgroundImage = "url('./img/car.gif')";
     }
   }
 })
