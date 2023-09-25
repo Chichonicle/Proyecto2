b768d982e90e68463ec7fa8daa54f4b64cdc6f4a
Christian <christianperez.rbb@gmail.com>
2023-09-25 02:59:40 +0200

    Parece que no se carga la carpeta de las imagenes, aunque en el repositorio si aparece, hago una prueba

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 let pantalla = document.getElementById("screen");
 let luz = document.getElementById("led");
-let botonPower = document.getElementById("idpower"); 
-let pantallaEncendida = false; 
+let botonPower = document.getElementById("idpower");
+let pantallaEncendida = false;
 
 botonPower.addEventListener("click", () => {
   if (!pantallaEncendida) {
-    
+
     pantalla.style.backgroundImage = "url('../img/on.jpg')";
     luz.style.backgroundColor = "yellow";
-    pantallaEncendida = true; 
+    pantallaEncendida = true;
     pantalla.addEventListener("animationiteration", () => {
       pantalla.style.animationPlayState = "paused";
     });
   } else {
     
-    pantalla.style.backgroundImage = "none"; 
-    luz.style.backgroundColor = ""; 
-    pantallaEncendida = false; 
+    pantalla.style.backgroundImage = "none";
+    luz.style.backgroundColor = "";
+    pantallaEncendida = false;
   }
 });
 
 document.addEventListener("click", (e) => {
-  
+
   if (pantallaEncendida) {
     if (e.target.id === "triangulo") {
       pantalla.style.backgroundImage = "url('../img/oliver.gif')";
@@ -33,6 +33,5 @@ document.addEventListener("click", (e) => {
       pantalla.style.backgroundImage = "url('../img/car.gif')";
     }
   }
-});
-
-
+})
+;
