9e81b9ff3f2df7255f3528491989495940e5a5c1
Christian <christianperez.rbb@gmail.com>
2023-09-25 02:02:55 +0200

    Consigo que el boton que enciende la consola tambien la apague y añado todas las imagenes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 let pantalla = document.getElementById("screen");
+let luz = document.getElementById("led");
+let botonPower = document.getElementById("idpower"); 
+let pantallaEncendida = false; 
 
-document.addEventListener("click", (e) => {
-  if (e.target.id === "triangulo") {
-    pantalla.style.backgroundColor = "red";
-  } else if (e.target.id === "idpower") {
-    pantalla.style.backgroundColor = "pink";
+botonPower.addEventListener("click", () => {
+  if (!pantallaEncendida) {
+    
+    pantalla.style.backgroundImage = "url('../img/on.jpg')";
+    luz.style.backgroundColor = "yellow";
+    pantallaEncendida = true; 
+    pantalla.addEventListener("animationiteration", () => {
+      pantalla.style.animationPlayState = "paused";
+    });
+  } else {
+    
+    pantalla.style.backgroundImage = "none"; 
+    luz.style.backgroundColor = ""; 
+    pantallaEncendida = false; 
   }
 });
+
+document.addEventListener("click", (e) => {
+  
+  if (pantallaEncendida) {
+    if (e.target.id === "triangulo") {
+      pantalla.style.backgroundImage = "url('../img/oliver.gif')";
+    } else if (e.target.id === "cuadrado") {
+      pantalla.style.backgroundImage = "url('../img/mk.gif')";
+    } else if (e.target.id === "circulo") {
+      pantalla.style.backgroundImage = "url('../img/sonic.gif')";
+    } else if (e.target.id === "x") {
+      pantalla.style.backgroundImage = "url('../img/car.gif')";
+    }
+  }
+});
+
+
